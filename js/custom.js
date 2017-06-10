063535a82d6aa536687c5939f119ad68a893d200
mharkess <michaelharkess@gmail.com>
2017-06-10 17:32:10 +0100

    new timezone code for testing in different locality

--- js/custom.js
+++ js/custom.js
@@ -7,24 +7,42 @@
 $(function() {
   $('#choose-cpu').hide();
 })
-    function showTheTime() {
-		//set current time according to UTC timezone
-        var s = moment().tz("Atlantic/Azores").format();
-        return s
-    }
-	
-	function expiryTime() {
-		//set expiry time according to UTC timezone.
-		var e = moment("2017-06-17 23:00").tz("Atlantic/Azores").format()
-		return e
-	}
-	
+
+ getcurrenttime = function() {
+   var local, utc, show, timezone
+
+   local = new Date();
+   timezone = 0
+   // get the current time in the show's timezone
+   utc = local.getTime() + (local.getTimezoneOffset() * 60000);
+   current = new Date(utc + (3600000*timezone));
+ return current
+}
+
+getexpirytime = function() {
+  var local, utc, show, timezone
+
+  local = new Date();
+  local.setUTCDate(17);
+  local.setHours(23,00,00,00)
+  timezone = 0
+  // get the current time in the show's timezone
+  utc = local.getTime() + (local.getTimezoneOffset() * 60000);
+  expiry = new Date(utc + (3600000*timezone));
+return expiry
+
+}
+
+
  window.setInterval(function(){
-    var current = showTheTime();
-    var expiry  = expiryTime()
+    var current = getcurrenttime();
+    var expiry  = getexpirytime()
       if(current < expiry) {
         $('#choose-cpu').hide();
         $('#hideme').show();
+        console.log(current)
+        console.log(expiry)
+
       }
       else {
         $('#choose-cpu').show();
