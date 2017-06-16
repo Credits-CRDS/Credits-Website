445fa6263a56bd175a570e84c4f6c1af6a715729
mharkess <michaelharkess@gmail.com>
2017-06-16 20:15:27 +0100

    show/hide jquery add

--- js/custom.js
+++ js/custom.js
@@ -3,47 +3,59 @@
  *
  * ------------------------------------------------------------------- */
 
-
 $(function() {
-  $('#choose-cpu').hide();
+
+  $(function() {
+    $('#choose-cpu').hide();
+  })
+
+   getcurrenttime = function() {
+     var local, utc, show, timezone
+
+     local = new Date();
+     timezone = 0
+     // get the current time in the show's timezone
+     utc = local.getTime() + (local.getTimezoneOffset() * 60000);
+     current = new Date(utc + (3600000*timezone));
+   return current
+  }
+
+  getexpirytime = function() {
+    var local, utc, show, timezone
+
+    local = new Date();
+    timezone = 0
+    // get the current time in the show's timezone
+    utc = local.getTime() + (local.getTimezoneOffset() * 60000);
+    expiry = new Date(utc + (3600000*timezone));
+    expiry.setUTCDate(17)
+    expiry.setHours(22,00,00,00)
+  return expiry
+
+  }
+
+  showHideTime = function() {
+      var x = document.getElementById('timezone');
+      if (x.style.display === 'none') {
+          x.style.display = 'block';
+      } else {
+          x.style.display = 'none';
+      }
+  }
+
+
+   window.setInterval(function(){
+      var current = getcurrenttime();
+      var expiry  = getexpirytime()
+        if(current < expiry) {
+          $('#choose-cpu').hide();
+          $('#hideme').show();
+
+        }
+        else {
+          $('#choose-cpu').show();
+          $('#hideme').hide();
+        }
+     }, 5000);
+
 })
-
- getcurrenttime = function() {
-   var local, utc, show, timezone
-
-   local = new Date();
-   timezone = 0
-   // get the current time in the show's timezone
-   utc = local.getTime() + (local.getTimezoneOffset() * 60000);
-   current = new Date(utc + (3600000*timezone));
- return current
-}
-
-getexpirytime = function() {
-  var local, utc, show, timezone
-
-  local = new Date();
-  timezone = 0
-  // get the current time in the show's timezone
-  utc = local.getTime() + (local.getTimezoneOffset() * 60000);
-  expiry = new Date(utc + (3600000*timezone));
-  expiry.setUTCDate(17)
-  expiry.setHours(22,00,00,00)
-return expiry
-
-}
-
-
- window.setInterval(function(){
-    var current = getcurrenttime();
-    var expiry  = getexpirytime()
-      if(current < expiry) {
-        $('#choose-cpu').hide();
-        $('#hideme').show();
-
-      }
-      else {
-        $('#choose-cpu').show();
-        $('#hideme').hide();
-      }
-   }, 5000);
